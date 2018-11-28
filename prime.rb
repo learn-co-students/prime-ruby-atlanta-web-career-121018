3eb37c65ade7745ec832f8706bccdbb64edef32a
Arthur Torres <arthurtorres002@gmail.com>
2018-11-28 15:38:28 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,9 +1,8 @@
 
 def prime?(number)
-  answer = true
-  answer = false if number <= 1
-  for test_number in 2..number
-    answer = false if (number != test_number && number % test_number == 0)
+  return false if number <= 1
+  for test_number in 2..(number - 1)
+    return false if number % test_number == 0
   end
-  answer
+  true
 end
