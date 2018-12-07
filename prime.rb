70ab3818349d8c2ec08a20b6a2c43fcb10e85404
Ryan Riesenberger <rjriesenberger@gmail.com>
2018-12-07 01:04:27 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+
+def prime? (number)
+  if number < 2
+    return false
+  end
+  divisor = 2
+  while (number > divisor)
+    if number % divisor == 0
+      return false
+    else
+      divisor += 1
+    end
+  end
+  return true
+end
+
+# def prime? (number)
+#   divisor = 2
+#   while (number > divisor)
+#     if (number % divisor == 0) || (number < 0)
+#       return false
+#     else
+#       divisor += 1
+#     end
+#   end
+#   return true
+# end
