3a6dcf4e5a3335893e1d1776d646bf882ecce00e
Chanda Grayson <chandagrayson@gmail.com>
2018-12-25 13:29:50 -0500

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
-# Add  code here!
+
+
+
+
+def prime?(number)
+    if number < 2
+        return false
+    end
+#
+    integer = 2
+    while integer <= number / 2
+      if number % integer == 0
+        return false
+      end
+      integer +=1
+    end
+    return true
+  end
+
+#     integer +=1
+#     limit = number / integer
+#     while integer >= limit
+#       if number % integer == 0
+#       end
+#     return true
+#     end
+# end
